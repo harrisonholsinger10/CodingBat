27647f44145cedf91cfca9e850966b841d430b24
Harrisonholsinger <harrisonholsinger@chenmed.com>
2022-11-01 15:17:31 -0400

    Warmup-1

---
+++ Warmup-1/monkeyTrouble.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class monkeyTrouble
+{
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile)
+    {
+
+        if (aSmile == true && bSmile == true)
+        {
+            return true;
+        }
+        else if (aSmile == false && bSmile == false)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+}
