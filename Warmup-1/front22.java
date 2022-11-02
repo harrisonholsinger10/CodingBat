150e91f4bdb4d6cc91f5392c98ac606738c1d2be
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-02 14:37:01 -0400

    Update front22.java

--- Warmup-1/front22.java
+++ Warmup-1/front22.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so "kitten" yields"kikittenki". 
+If the string length is less than 2, use whatever chars are there.
+*/
+
 public class front22
 {
     public String front22(String str)
