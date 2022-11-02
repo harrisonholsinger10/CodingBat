e57fb8c299ebffee0b45a4545893d2e7c89207d1
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-02 13:58:40 -0400

    Update nearHundred.java

--- Warmup-1/nearHundred.java
+++ Warmup-1/nearHundred.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+/*
+Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
+*/
+
 public class nearHundred
 {
     public boolean nearHundred(int n)
