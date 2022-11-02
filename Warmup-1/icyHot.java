09a8f39db187208d0279e5ab968db0e783a08d47
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-02 14:38:32 -0400

    Update icyHot.java

--- Warmup-1/icyHot.java
+++ Warmup-1/icyHot.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+/*
+Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+*/
+
 public class icyHot
 {
     public boolean icyHot(int temp1, int temp2)
