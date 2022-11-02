c4cce436086409bfe77dbed2f10c5e8346726daf
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-02 14:46:28 -0400

    Update loneTeen.java

--- Warmup-1/loneTeen.java
+++ Warmup-1/loneTeen.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+/*
+We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 2 int values, return true if one or the other is teen, but not both.
+*/
+
 public class loneTeen
 {
     public boolean loneTeen(int a, int b) {
