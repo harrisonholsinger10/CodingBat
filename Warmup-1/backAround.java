fc8b70572d925fe0db2488b0e8efbd8a5385cb2d
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-02 14:34:50 -0400

    Update backAround.java

--- Warmup-1/backAround.java
+++ Warmup-1/backAround.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+Given a string, take the last char and return a new string with the last char added at the front and back, so "cat" yields "tcatt". 
+The original string will be length 1 or more.
+*/
+
 public class backAround
 {
     public String backAround(String str)
