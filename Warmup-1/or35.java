827ee8bc73bac96b35142e083c0294cdb54d484d
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-02 14:36:22 -0400

    Update or35.java

--- Warmup-1/or35.java
+++ Warmup-1/or35.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+/*
+Return true if the given non-negative number is a multiple of 3 or a multiple of 5. Use the % "mod" operator
+*/
+
 public class or35
 {
     public boolean or35(int n)
