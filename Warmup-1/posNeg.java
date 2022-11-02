fab48aa253b86ef73ccef7f94efa81d576d2ca21
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-02 13:59:26 -0400

    Update posNeg.java

--- Warmup-1/posNeg.java
+++ Warmup-1/posNeg.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+/*
+Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, then return true only if both are negative.
+*/
 public class posNeg
 {
     public boolean posNeg(int a, int b, boolean negative)
