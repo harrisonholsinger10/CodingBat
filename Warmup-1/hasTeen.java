155804a6474aa237e5eb494544c4bb445a9d3fcb
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-02 14:45:50 -0400

    Update hasTeen.java

--- Warmup-1/hasTeen.java
+++ Warmup-1/hasTeen.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+/*
+We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 3 int values, return true if 1 or more of them are teen.
+*/
+
 public class hasTeen
 {
     public boolean hasTeen(int a, int b, int c)
