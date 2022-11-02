1f2ab573e8514fe707b8e52b25578e0521b3046a
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-02 14:33:56 -0400

    Update front3.java

--- Warmup-1/front3.java
+++ Warmup-1/front3.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+Given a string, we'll say that the front is the first 3 chars of the string. 
+If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+*/
+
 public class front3
 {
     public String front3(String str)
