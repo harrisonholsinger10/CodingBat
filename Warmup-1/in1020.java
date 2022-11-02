46fbaa9986bb7cd1c7fbdf67a9767f7ea93ad173
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-02 14:39:38 -0400

    Update in1020.java

--- Warmup-1/in1020.java
+++ Warmup-1/in1020.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+/*
+Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+*/
+
 public class in1020
 {
     public boolean in1020(int a, int b)
