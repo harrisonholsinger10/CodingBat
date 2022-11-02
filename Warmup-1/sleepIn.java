698ee9e364b23cc58ce16842c7e2ed95c60b07c3
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-02 13:54:34 -0400

    Update sleepIn.java

--- Warmup-1/sleepIn.java
+++ Warmup-1/sleepIn.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
-/* The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. 
+/* 
+The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. 
 We sleep in if it is not a weekday or we're on vacation. 
-Return true if we sleep in. */
+Return true if we sleep in. 
+*/
 
 public class sleepIn
 {
