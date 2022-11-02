025cdfcefd465460d3332cdc2da813c91fb661aa
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-02 14:02:50 -0400

    Update missingChar.java

--- Warmup-1/missingChar.java
+++ Warmup-1/missingChar.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+Given a non-empty string and an int n, return a new string where the char at index n has been removed. 
+The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..str.length()-1 inclusive).
+*/
+
 public class missingChar
 {
     public String missingChar(String str, int n)
