b4f6a5da6efd1fa278062d0ac162890893f206ad
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-11 17:40:12 -0500

    Map-2

--- Map-2/word0.java
+++ Map-2/word0.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+/*
+Given an array of strings, return a Map<String, Integer> containing a key for every different string in the array, 
+always with the value 0. For example the string "hello" makes the pair "hello":0. We'll do more complicated counting later, 
+but for this problem the value is simply 0.
+*/
+
 import java.util.HashMap;
 import java.util.Map;
 
