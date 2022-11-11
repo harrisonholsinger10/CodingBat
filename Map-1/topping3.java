0691cac5f87b175b91088cdfdd86f9fdfd54b1a4
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-11 17:35:49 -0500

    Map-1

--- Map-1/topping3.java
+++ Map-1/topping3.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+/*
+Given a map of food keys and topping values, modify and return the map as follows: 
+if the key "potato" has a value, set that as the value for the key "fries". 
+If the key "salad" has a value, set that as the value for the key "spinach".
+*/
+
 import java.util.Map;
 
 public class topping3
