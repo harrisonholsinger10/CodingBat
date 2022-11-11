cbba7eb40d8430b01e099ecd49fe8d1691f3a8cf
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-11 17:14:52 -0500

    Warmup-2

--- Warmup-2/countXX.java
+++ Warmup-2/countXX.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+/*
+Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+*/
+
 public class countXX
 {
     int countXX(String str)
