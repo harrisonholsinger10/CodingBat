0f0f139a79fd7eeff655941887131c8383737002
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-11 17:42:41 -0500

    Map-2

--- Map-2/firstChar.java
+++ Map-2/firstChar.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+Given an array of non-empty strings, return a Map<String, String> with a key for every different first character seen, 
+with the value of all the strings starting with that character appended together in the order they appear in the array.
+*/
+
 import java.util.HashMap;
 import java.util.Map;
 
