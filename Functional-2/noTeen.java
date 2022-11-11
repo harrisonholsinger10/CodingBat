5df8f3cbdb2ff3920541bbf320d1440faaead73d
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-11 18:05:46 -0500

    Functional-2

--- Functional-2/noTeen.java
+++ Functional-2/noTeen.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+/*
+* Given a list of integers, return a list of those numbers, omitting any that are between 13 and 19 inclusive.
+*/
+
 import java.util.List;
 import java.util.stream.Collectors;
 
