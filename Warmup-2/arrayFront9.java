fa001c422730cc25cf0070b54e33930dcff6a744
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-11 17:19:16 -0500

    Warmup-2

--- Warmup-2/arrayFront9.java
+++ Warmup-2/arrayFront9.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+/*
+Given an array of ints, return true if one of the first 4 elements in the array is a 9. The array length may be less than 4.
+*/
+
 public class arrayFront9
 {
     public boolean arrayFront9(Integer[] nums)
