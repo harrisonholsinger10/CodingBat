49f2c11987ee206abeb0f544bf944fab72eb96fe
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-11 17:34:51 -0500

    Map-1

--- Map-1/topping1.java
+++ Map-1/topping1.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+Given a map of food keys and topping values, modify and return the map as follows: 
+if the key "ice cream" is present, set its value to "cherry". In all cases, set the key "bread" to have the value "butter".
+*/
+
 import java.util.Map;
 
 public class topping1
