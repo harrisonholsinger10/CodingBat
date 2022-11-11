44cd2b422bbaa15d9ba9ae13c83ee464b40aece2
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-11 17:14:11 -0500

    Warmup-2

--- Warmup-2/frontTimes.java
+++ Warmup-2/frontTimes.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+/*
+Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+*/
+
 public class frontTimes
 {
     public String frontTimes(String str, int n)
