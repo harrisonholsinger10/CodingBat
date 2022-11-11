d21f8fe92e9cf9ae494d25399255d85c9d96a287
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-11 17:23:42 -0500

    Warmup-2

--- Warmup-2/array667.java
+++ Warmup-2/array667.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+Given an array of ints, return the number of times that two 6's are next to each other in the array. 
+Also count instances where the second "6" is actually a 7.
+*/
+
 public class array667
 {
     public int array667(int[] nums)
