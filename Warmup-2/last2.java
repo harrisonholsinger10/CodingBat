1860d35d14cb4bdfb2432dcdcdca7fd984e2dbd6
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-11 17:18:18 -0500

    Warmup-2

--- Warmup-2/last2.java
+++ Warmup-2/last2.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+/*
+Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+*/
+
 public class last2
 {
     public int last2(String str)
