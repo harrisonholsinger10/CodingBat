35ece0805776bf2a5feb0d26531d21839e655031
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-11 18:01:51 -0500

    Functional-1

--- Fuctional-1/math1.java
+++ Fuctional-1/math1.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+/*
+Given a list of integers, return a list where each integer is added to 1 and the result is multiplied by 10.
+*/
+
 import java.util.List;
 import java.util.stream.Collectors;
 
