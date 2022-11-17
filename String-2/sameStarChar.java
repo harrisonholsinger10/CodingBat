c185c8b7d9b0497f3ab41a298d3a5acb1c574544
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 13:42:37 -0500

    String-2

--- String-2/sameStarChar.java
+++ String-2/sameStarChar.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* Returns true if for every '*' (star) in the string, 
+* if there are chars both immediately before and after the star, they are the same.
+*/
+
 public class sameStarChar
 {
     public boolean sameStarChar(String str)
