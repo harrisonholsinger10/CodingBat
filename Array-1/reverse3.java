ed176649b76b6ad9659fcf9722d565fe746f8922
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 10:56:02 -0500

    Array-1

--- Array-1/reverse3.java
+++ Array-1/reverse3.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+/*
+* Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
+*/
+
 public class reverse3
 {
     public int[] reverse3(int[] nums)
