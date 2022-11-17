6a7e0f3966d56ed53749514effe45208c9fae8d7
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 11:03:58 -0500

    Array-1

--- Array-1/double23.java
+++ Array-1/double23.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+/*
+* Given an int array, return true if the array contains 2 twice, or 3 twice. The array will be length 0, 1, or 2.
+*/
+
 public class double23
 {
     public boolean double23(int[] nums)
