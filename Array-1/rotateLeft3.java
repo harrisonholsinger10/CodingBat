9dd8527f13761b3d3a8de32e4510446a7112fdbc
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 10:55:19 -0500

    Array-1

--- Array-1/rotateLeft3.java
+++ Array-1/rotateLeft3.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+/*
+* Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+*/
+
 public class rotateLeft3
 {
     public int[] rotateLeft3(int[] nums)
