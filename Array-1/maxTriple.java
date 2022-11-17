8032572ddcf2bf54c50985c245b6d0b4ba5b33b2
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 11:09:20 -0500

    Array-1

--- Array-1/maxTriple.java
+++ Array-1/maxTriple.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* Given an array of ints of odd length, look at the first, last, and middle values in the array and return the largest. 
+* The array length will be a least 1.
+*/
+
 public class maxTriple
 {
     public int maxTriple(int[] nums)
