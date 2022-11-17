a18faab93393ece4e5f4b0aa8eb18469ac123165
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 11:10:27 -0500

    Array-1

--- Array-1/unlucky1.java
+++ Array-1/unlucky1.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* We'll say that a 1 immediately followed by a 3 in an array is an "unlucky" 1. 
+* Return true if the given array contains an unlucky 1 in the first 2 or last 2 positions in the array.
+*/
+
 public class unlucky1
 {
     public boolean unlucky1(int[] nums)
