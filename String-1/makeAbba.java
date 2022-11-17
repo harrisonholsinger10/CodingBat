5de667031e82126c9cfff1ec2ea3e44a0674a908
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 11:18:18 -0500

    String-1

--- String-1/makeAbba.java
+++ String-1/makeAbba.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* Given two strings, a and b, return the result of putting them together in the order abba, 
+* e.g. "Hi" and "Bye" returns "HiByeByeHi".
+*/
+
 public class makeAbba
 {
     public String makeAbba(String a, String b)
