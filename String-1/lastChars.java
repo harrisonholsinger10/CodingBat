39113cd09011d8efe8b0ead80b0bf92c43faaca8
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 12:37:15 -0500

    String-1

--- String-1/lastChars.java
+++ String-1/lastChars.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* Given 2 strings, a and b, return a new string made of the first char of a and the last char of b, 
+* so "yo" and "java" yields "ya". If either string is length 0, use '@' for its missing char.
+*/
+
 public class lastChars
 {
     public String lastChars(String a, String b)
