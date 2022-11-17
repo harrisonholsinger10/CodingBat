46d8758c1bdb9bbe64a1ed50231f96bd47e6fd9c
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 13:31:00 -0500

    String-2

--- String-2/xyzThere.java
+++ String-2/xyzThere.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* Return true if the given string contains an appearance of "xyz" where the xyz is not directly preceeded by a period (.). 
+* So "xxyz" counts but "x.xyz" does not.
+*/
+
 public class xyzThere
 {
     public boolean xyzThere(String str)
