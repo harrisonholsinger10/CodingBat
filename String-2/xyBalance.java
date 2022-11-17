392d0a6116f2fe245ff3c7d628073950e9f4fc72
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 13:32:36 -0500

    String-2

--- String-2/xyBalance.java
+++ String-2/xyBalance.java
@@ -1,5 +1,12 @@
 package com.example.demo;
 
+/*
+* We'll say that a String is xy-balanced if for all the 'x' chars in the string, 
+* there exists a 'y' char somewhere later in the string. So "xxy" is balanced, 
+* but "xyx" is not. One 'y' can balance multiple 'x's. 
+* Return true if the given string is xy-balanced.
+*/
+
 public class xyBalance
 {
     public boolean xyBalance(String str)
