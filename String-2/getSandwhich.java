11281682687223ab6e4756022110e032a9799145
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 13:42:00 -0500

    String-2

--- String-2/getSandwhich.java
+++ String-2/getSandwhich.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+/*
+* A sandwich is two pieces of bread with something in between. 
+* Return the string that is between the first and last appearance of "bread" in the given string, 
+* or return the empty string "" if there are not two pieces of bread.
+*/
+
 public class getSandwhich
 {
     public String getSandwich(String str)
