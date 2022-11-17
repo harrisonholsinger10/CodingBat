4053b53049fb82e62f69e4e76d6e3c0fab752de6
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 13:54:26 -0500

    Logic-2

--- Logic-2/makeChocolate.java
+++ Logic-2/makeChocolate.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* We want make a package of goal kilos of chocolate. We have small bars (1 kilo each) and big bars (5 kilos each). 
+* Return the number of small bars to use, assuming we always use big bars before small bars. Return -1 if it can't be done.
+*/
+
 public class makeChocolate
 {
     public int makeChocolate(int small, int big, int goal)
