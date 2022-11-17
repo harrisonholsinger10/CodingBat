2596642fe9048183addc229cb49d466bc9db315f
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 11:04:27 -0500

    Array-1

--- Array-1/fix23.java
+++ Array-1/fix23.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* Given an int array length 3, if there is a 2 in the array immediately followed by a 3, 
+* set the 3 element to 0. Return the changed array.
+*/
+
 public class fix23
 {
     public int[] fix23(int[] nums)
