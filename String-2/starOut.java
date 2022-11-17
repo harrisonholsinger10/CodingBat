47ace1c7b46b2c662ff6cd58e749c87fb07ca461
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 13:44:23 -0500

    String-2

--- String-2/starOut.java
+++ String-2/starOut.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+/*
+* Return a version of the given string, where for every star (*) in the string 
+* the star and the chars immediately to its left and right are gone. 
+* So "ab*cd" yields "ad" and "ab**cd" also yields "ad".
+*/
+
 public class starOut
 {
     public String starOut(String str)
