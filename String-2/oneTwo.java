6d09fd40ef59aaf9c712b1365dec30faf2c4eb18
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 13:43:09 -0500

    String-2

--- String-2/oneTwo.java
+++ String-2/oneTwo.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+/*
+* Given a string, compute a new string by moving the first char to come after the next two chars, 
+* so "abc" yields "bca". Repeat this process for each subsequent group of 3 chars, so "abcdef" yields "bcaefd". 
+* Ignore any group of fewer than 3 chars at the end.
+*/
+
 public class oneTwo
 {
     public String oneTwo(String str)
