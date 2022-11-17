0e1e350c6cf0aaac7ece850eaa4d5e4bc59d0754
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 13:48:19 -0500

    Logic-2

--- Logic-2/loneSum.java
+++ Logic-2/loneSum.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+/*
+* Given 3 int values, a b c, return their sum. However, 
+* if one of the values is the same as another of the values, 
+* it does not count towards the sum.
+*/
+
 public class loneSum
 {
     public int loneSum(int a, int b, int c)
