def044484b2435fc4a7bfc09dc7055f3f6923677
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 12:35:45 -0500

    String-1

--- String-1/middleThree.java
+++ String-1/middleThree.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* Given a string of odd length, return the string length 3 from its middle, 
+* so "Candy" yields "and". The string length will be at least 3.
+*/
+
 public class middleThree
 {
     public String middleThree(String str)
