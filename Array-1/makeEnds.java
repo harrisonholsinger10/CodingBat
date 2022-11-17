83c8df773ccdd043709f5025ec5d5450b84c7a0c
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 11:14:09 -0500

    Array-1

--- Array-1/makeEnds.java
+++ Array-1/makeEnds.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* Given an array of ints, return a new array length 2 containing the first and last elements from the original array. 
+* The original array will be length 1 or more.
+*/
+
 public class makeEnds
 {
     public int[] makeEnds(int[] nums)
