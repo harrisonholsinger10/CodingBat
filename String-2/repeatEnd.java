6d22aff8ee53da5a4c2815d786831e5f1539bf76
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 13:33:46 -0500

    String-2

--- String-2/repeatEnd.java
+++ String-2/repeatEnd.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* Given a string and an int n, return a string made of n repetitions of the last n characters of the string. 
+* You may assume that n is between 0 and the length of the string, inclusive.
+*/
+
 public class repeatEnd
 {
     public String repeatEnd(String str, int n)
