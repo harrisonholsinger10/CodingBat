8f4b75c9dc1ee9740b35d17baf791ad21794a428
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 13:53:53 -0500

    Logic-2

--- Logic-2/evenlySpaced.java
+++ Logic-2/evenlySpaced.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+/*
+* Given three ints, a b c, one of them is small, one is medium and one is large. 
+* Return true if the three values are evenly spaced, 
+* so the difference between small and medium is the same as the difference between medium and large.
+*/
+
 public class evenlySpaced
 {
     public boolean evenlySpaced(int a, int b, int c)
