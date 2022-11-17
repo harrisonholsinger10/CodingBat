518720dc35e9fe4c2ce0417a9c8ea0969a395adb
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 13:52:54 -0500

    Logic-2

--- Logic-2/closeFar.java
+++ Logic-2/closeFar.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+/*
+* Given three ints, a b c, return true if one of b or c is "close" (differing from a by at most 1), 
+* while the other is "far", differing from both other values by 2 or more. 
+* Note: Math.abs(num) computes the absolute value of a number.
+*/
+
 public class closeFar
 {
     public boolean closeFar(int a, int b, int c)
