e171d61b581cb36b8fbb3dcf9850a35b085bdf5c
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 12:51:24 -0500

    String-1

--- String-1/withoutX2.java
+++ String-1/withoutX2.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+/*
+* Given a string, if one or both of the first 2 chars is 'x', 
+* return the string without those 'x' chars, and otherwise return the string unchanged. 
+* This is a little harder than it looks.
+*/
+
 public class withoutX2
 {
     public String withoutX2(String str)
