449a1f02b02531425dc4be8029bf8036a529196b
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 13:34:29 -0500

    String-2

--- String-2/repeatFront.java
+++ String-2/repeatFront.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+/*
+* Given a string and an int n, return a string made of the first n characters of the string, 
+* followed by the first n-1 characters of the string, and so on. You may assume that n is between 0 and the length of the string, 
+* inclusive (i.e. n >= 0 and n <= str.length()).
+*/
+
 public class repeatFront
 {
     public String repeatFront(String str, int n)
