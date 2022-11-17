9022fe860cbadafa3f23143f8f2e4eb76138c64b
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 12:36:46 -0500

    String-1

--- String-1/atFirst.java
+++ String-1/atFirst.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* Given a string, return a string length 2 made of its first 2 chars. 
+* If the string length is less than 2, use '@' for the missing chars.
+*/
+
 public class atFirst
 {
     public String atFirst(String str)
