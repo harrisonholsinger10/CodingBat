a9e25b9c72a85da785ad28bc2613a060a87c46e8
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 11:08:09 -0500

    Array-1

--- Array-1/swapEnds.java
+++ Array-1/swapEnds.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* Given an array of ints, swap the first and last elements in the array. Return the modified array. 
+* The array length will be at least 1.
+*/
+
 public class swapEnds
 {
     public int[] swapEnds(int[] nums)
