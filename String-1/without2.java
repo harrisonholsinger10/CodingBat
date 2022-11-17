872fc8d6c9cf7ece6f431aa1f798f0b916e4724e
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 12:41:05 -0500

    String-1

--- String-1/without2.java
+++ String-1/without2.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+/*
+* Given a string, if a length 2 substring appears at both its beginning and end, 
+* return a string without the substring at the beginning, so "HelloHe" yields "lloHe". 
+* The substring may overlap with itself, so "Hi" yields "". Otherwise, return the original string unchanged.
+*/
+
 public class without2
 {
     public String without2(String str)
