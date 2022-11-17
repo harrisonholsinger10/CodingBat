515854b58b537735a9e594bcb075a773b044029e
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 13:30:16 -0500

    String-2

--- String-2/endOther.java
+++ String-2/endOther.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+/*
+* Given two strings, return true if either of the strings appears at the very end of the other string, 
+* ignoring upper/lower case differences (in other words, the computation should not be "case sensitive"). 
+* Note: str.toLowerCase() returns the lowercase version of a string.
+*/
+
 public class endOther
 {
     public boolean endOther(String a, String b)
