7070847ed6f9b28b9fbb174f7c5b7dfc50ffd0dd
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-17 13:29:35 -0500

    String-2

--- String-2/countCode.java
+++ String-2/countCode.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* Return the number of times that the string "code" appears anywhere in the given string, 
+* except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+*/
+
 public class countCode
 {
     public int countCode(String str)
