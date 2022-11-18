2372261b284746b0b872afdb81275bf300d52fb3
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-18 13:53:29 -0500

    Logic-1

--- Logic-1/specialEleven.java
+++ Logic-1/specialEleven.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+/*
+* We'll say a number is special if it is a multiple of 11 or if it is one more than a multiple of 11. 
+* Return true if the given non-negative number is special. Use the % "mod" operato
+*/
 public class specialEleven
 {
     public boolean specialEleven(int n)
