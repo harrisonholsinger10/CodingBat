8a59388f51eac2f4b11a8cbc2eee7d231375311b
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-18 14:01:34 -0500

    Logic-1

--- Logic-1/fizzString.java
+++ Logic-1/fizzString.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+/*
+* Given a string str, if the string starts with "f" return "Fizz". 
+* If the string ends with "b" return "Buzz". If both the "f" and "b" conditions are true, 
+* return "FizzBuzz". In all other cases, return the string unchanged. 
+*/
+
 public class fizzString
 {
     public String fizzString(String str)
