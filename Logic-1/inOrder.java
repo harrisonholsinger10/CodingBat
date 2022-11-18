4d6f6c14342b1abb50dca56d89599ff1da0bf708
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-18 14:03:32 -0500

    Logic-1

--- Logic-1/inOrder.java
+++ Logic-1/inOrder.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* Given three ints, a b c, return true if b is greater than a, and c is greater than b. 
+* However, with the exception that if "bOk" is true, b does not need to be greater than a.
+*/
+
 public class inOrder
 {
     public boolean inOrder(int a, int b, int c, boolean bOk)
