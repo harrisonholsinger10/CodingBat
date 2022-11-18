aa82939df72d5dc3b4e1b7a48cdd6008a3d88f7c
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-18 13:49:26 -0500

    Logic-1

--- Logic-1/sortaSum.java
+++ Logic-1/sortaSum.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, 
+* are forbidden, so in that case just return 20.
+*/
+
 public class sortaSum
 {
     public int sortaSum(int a, int b)
