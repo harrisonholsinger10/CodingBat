703f618c1ddb84dbfc2f75101b69ca321753d206
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-18 13:52:43 -0500

    Logic-1

--- Logic-1/in1To10.java
+++ Logic-1/in1To10.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+/*
+* Given a number n, return true if n is in the range 1..10, inclusive. 
+* Unless outsideMode is true, in which case return true if the number is less or equal to 1, 
+* or greater or equal to 10.
+*/
+
 public class in1To10
 {
     public boolean in1To10(int n, boolean outsideMode)
