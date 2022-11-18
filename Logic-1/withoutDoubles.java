5fd9bd0c4d58efbb63d1c0ea97acf3957ec99ad6
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-18 14:05:58 -0500

    Logic-1

--- Logic-1/withoutDoubles.java
+++ Logic-1/withoutDoubles.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+/*
+* Return the sum of two 6-sided dice rolls, each in the range 1..6. 
+* However, if noDoubles is true, if the two dice show the same value, 
+* increment one die to the next value, wrapping around to 1 if its value was 6.
+*/
+
 public class withoutDoubles
 {
     public int withoutDoubles(int die1, int die2, boolean noDoubles)
