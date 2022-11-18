45a9b930486ff12117534ef29050cc22644cf4f8
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-18 13:52:12 -0500

    Logic-1

--- Logic-1/love6.java
+++ Logic-1/love6.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+/*
+* The number 6 is a truly great number. Given two int values, a and b, 
+* return true if either one is 6. Or if their sum or difference is 6. 
+* Note: the function Math.abs(num) computes the absolute value of a number.
+*/
+
 public class love6
 {
     public boolean love6(int a, int b)
