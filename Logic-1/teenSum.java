9283f1f945373a68c7b1b4cc25416db36a2f9423
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-18 13:59:12 -0500

    Logic-1

--- Logic-1/teenSum.java
+++ Logic-1/teenSum.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* Given 2 ints, a and b, return their sum. However, "teen" values in the range 13..19 inclusive, 
+* are extra lucky. So if either value is a teen, just return 19.
+*/
+
 public class teenSum
 {
     public int teenSum(int a, int b)
