f1926ef305ec5791c91d4a2ec61498494fa9d3ba
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-18 13:57:47 -0500

    Logic-1

--- Logic-1/less20.java
+++ Logic-1/less20.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* Return true if the given non-negative number is 1 or 2 less than a multiple of 20. 
+* So for example 38 and 39 return true, but 40 returns false.
+*/
+
 public class less20
 {
     public boolean less20(int n)
