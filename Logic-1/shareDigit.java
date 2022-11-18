f0f7a30873b788037fd7355a3ebec7e7cfc57f3f
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-18 14:09:22 -0500

    Logic-1

--- Logic-1/shareDigit.java
+++ Logic-1/shareDigit.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* Given two ints, each in the range 10..99, return true if there is a digit that appears in both numbers, 
+* such as the 2 in 12 and 23. (Note: division, e.g. n/10, gives the left digit while the % "mod" n%10 gives the right digit.)
+*/
+
 public class shareDigit
 {
     public boolean shareDigit(int a, int b)
