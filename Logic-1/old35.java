45e8e92acb0fe213f83b1dd1926c077a16070380
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-18 13:56:15 -0500

    Logic-1

--- Logic-1/old35.java
+++ Logic-1/old35.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/*
+* Return true if the given non-negative number is a multiple of 3 or 5, 
+* but not both. Use the % "mod" operator
+*/
+
 public class old35
 {
     public boolean old35(int n)
