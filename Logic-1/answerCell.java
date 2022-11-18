cc3f8894d126cc5c0fa0203c52e55921e1f9dfff
Harrison Holsinger <115578965+harrisonholsinger10@users.noreply.github.com>
2022-11-18 13:59:49 -0500

    Logic-1

--- Logic-1/answerCell.java
+++ Logic-1/answerCell.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+/*
+* Your cell phone rings. Return true if you should answer it. 
+* Normally you answer, except in the morning you only answer if it is your mom calling. 
+* In all cases, if you are asleep, you do not answer.
+*/
+
 public class answerCell
 {
     public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep)
